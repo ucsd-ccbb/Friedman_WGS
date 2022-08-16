f6e718135fc239f86c7006a57d2722a6c6d853d6
Ubuntu <ubuntu@ip-172-31-53-237.us-west-2.compute.internal>
2022-08-16 23:38:37 +0000

    norm_vep.sh operates on entire vcf, not by chr

--- scripts/norm_vep.sh
+++ scripts/norm_vep.sh
@@ -4,34 +4,29 @@
 export PATH=/shared/workspace/software/bcbio/anaconda/bin:/shared/workspace/software/bcbio/tools/bin:$PATH
 
 s3=s3://menieres-analysis-results/joint_genotype/2022-04-30_friedman_joint_metadata_04182022
-vcf=menieres-joint-gatk-haplotype-joint-annotated.chr"$chr".vep.vcf.gz
+vcf=menieres-joint-gatk-haplotype-joint-annotated-vqsr.vcf.gz
 fixed_vcf=$(echo $vcf | sed 's/vcf.gz/fixed.vcf.gz/')
 genome_fasta=/shared/workspace/software/bcbio/genomes/Hsapiens/hg38/seq/hg38.fa
 vep_cache=/shared/workspace/software/bcbio/genomes/Hsapiens/hg38/vep
 workspace=/scratch/$chr
 mkdir -p $workspace/
 
-if [ ! $workspace/$vcf ]; then
-	aws s3 cp $s3/$vcf $workspace/
-	aws s3 cp $s3/$vcf.tbi $workspace/
-fi
-if [ ! $workspace/$fixed_vcf ]; then
-	bcftools view -h $workspace/$vcf > $workspace/hdr.txt
-	sed -i -E -e 's/Number=1|Number=R|Number=A/Number=./g' $workspace/hdr.txt
-	bcftools reheader -h $workspace/hdr.txt -o $workspace/$fixed_vcf $workspace/$vcf
-	tabix -p vcf $workspace/$fixed_vcf
-fi
-if [ ! $workspace/menieres.norm.$chr.vcf.gz ]; then
-	bcftools norm -m - -c s --fasta-ref $genome_fasta -r chr$chr -Oz \
-	-o $workspace/menieres.norm.$chr.vcf.gz \
-	$workspace/$fixed_vcf
-	tabix -p vcf $workspace/menieres.norm.$chr.vcf.gz
-fi
+aws s3 cp $s3/$vcf $workspace/
+aws s3 cp $s3/$vcf.tbi $workspace/
 
-if [ ! $workspace/menieres.norm.$chr.vep.vcf.gz ]; then
-	tabix -h $workspace/menieres.norm.$chr.vcf.gz chr$chr | \
-	`which vep` \
-	-o $workspace/menieres.norm.$chr.vep.vcf \
+bcftools view -h $workspace/$vcf > $workspace/hdr.txt
+sed -i -E -e 's/Number=1|Number=R|Number=A/Number=./g' $workspace/hdr.txt
+bcftools reheader -h $workspace/hdr.txt -o $workspace/$fixed_vcf $workspace/$vcf
+tabix -p vcf $workspace/$fixed_vcf
+
+bcftools norm -m - -c s --fasta-ref $genome_fasta -Oz \
+-o $workspace/menieres-joint-gatk-haplotype-joint-annotated-vqsr-normed.vcf.gz \
+$workspace/$fixed_vcf
+tabix -p vcf $workspace/menieres-joint-gatk-haplotype-joint-annotated-vqsr-normed.vcf.gz
+
+`which vep` \
+	-i $workspace/menieres-joint-gatk-haplotype-joint-annotated-vqsr-normed.vcf.gz \
+	-o $workspace/menieres-joint-gatk-haplotype-joint-annotated-vqsr-normed-vep.vcf \
 	--vcf \
 	--verbose \
 	--cache \
@@ -52,8 +47,7 @@ if [ ! $workspace/menieres.norm.$chr.vep.vcf.gz ]; then
 	--appris --gene_phenotype --af \
 	--af_1kg --af_esp --af_gnomad \
 	--max_af --pubmed --variant_class --format vcf
-	bgzip $workspace/menieres.norm.$chr.vep.vcf
-	tabix -p vcf $workspace/menieres.norm.$chr.vep.vcf.gz
-	aws s3 cp $workspace/menieres.norm.$chr.vep.vcf.gz $s3/
-	aws s3 cp $workspace/menieres.norm.$chr.vep.vcf.gz.tbi $s3/
-fi
+bgzip $workspace/$workspace/menieres-joint-gatk-haplotype-joint-annotated-vqsr-normed-vep.vcf
+tabix -p vcf $workspace/$workspace/menieres-joint-gatk-haplotype-joint-annotated-vqsr-normed-vep.vcf.gz
+aws s3 cp $workspace/$workspace/menieres-joint-gatk-haplotype-joint-annotated-vqsr-normed-vep.vcf.gz $s3/
+aws s3 cp $workspace/$workspace/menieres-joint-gatk-haplotype-joint-annotated-vqsr-normed-vep.vcf.gz.tbi $s3/
